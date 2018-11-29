d5c4b0a474d4514e20d1aea6af45535b8e9700dc
郭昕楠 <guo.xinnan@geely.com>
2018-11-29 22:50:32 +0800

    xiugai

--- login/forms.py
+++ login/forms.py
@@ -6,7 +6,7 @@ from django import forms
 class UserForm(forms.Form):
     username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label="密码", max_length=256,  widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    captcha = CaptchaField(label='验证码')
+    # captcha = CaptchaField(label='验证码')
 
 
 class RegisterForm(forms.Form):
@@ -19,7 +19,7 @@ class RegisterForm(forms.Form):
     password2 = forms.CharField(label="确认密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label="邮箱地址", widget=forms.EmailInput(attrs={'class': 'form-control'}))
     sex = forms.ChoiceField(label='性别', choices=gender)
-    captcha = CaptchaField(label='验证码')
+    # captcha = CaptchaField(label='验证码')
 
 class DepartmentModelForm(forms.Form):
     dept_name = forms.CharField(label="部门名称", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '请输入部门！！！'}))
